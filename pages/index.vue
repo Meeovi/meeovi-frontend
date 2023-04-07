71b6caef4dc886aa178a5842b5e8312c8ce91a68
Sebastian hilton <bastianh86@outlook.com>
2023-04-06 22:00:30 -0700

    added social pages, updated UI, added graphql

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,32 @@
 <template>
     <div>
+        <headerslider />
         <callout />
         <callout2 />
         <features />
         <productList />
+        <relatedcreators />
         <simpleSingle />
     </div>
 </template>
 
 <script>
+    import headerslider from '../components/homePage/headerslider.vue'
     import callout from '../components/homePage/callout.vue'
     import callout2 from '../components/homePage/features.vue'
     import features from '../components/homePage/features.vue'
     import productList from '../components/homePage/productList.vue'
+    import relatedcreators from '../components/Creators/relatedcreators.vue'
     import simpleSingle from '../components/homePage/simpleSingle.vue'
 
     export default {
         components: {
+            headerslider,
             callout,
             callout2,
             features,
             productList,
+            relatedcreators,
             simpleSingle
         }
     }
