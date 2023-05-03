e7cf43f8811462898874396a26899acf9219a90d
Sebastian hilton <bastianh86@outlook.com>
2023-05-03 16:54:12 -0700

    updated lowerbar and added to UI

--- components/Menus/lowerbar.vue
+++ components/Menus/lowerbar.vue
@@ -13,6 +13,10 @@
             <v-tab><a href="/Departments/yardsale">Yardsale</a></v-tab>
             <v-tab><a href="/social/newsfeed">Newsfeed</a></v-tab>
             <v-tab><a href="/social/spaces">Spaces</a></v-tab>
+            <v-tab><a href="https://www.pixanomy.com">Sell Your Artwork</a></v-tab>
+            <v-tab><a href="https://www.shopncute.com">Shop For Her</a></v-tab>
+            <v-tab><a href="https://www.shopnbold.com">Shop For Him</a></v-tab>
+            <v-tab><a href="/commerce/buyagain">Buy Again</a></v-tab>
         </v-tabs>
     </v-card>
 </template>
