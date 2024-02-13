5736499372d14a60acd73a90d3f5ba1968be21ae
Sebastian hilton <bastianh86@outlook.com>
2024-02-13 03:24:20 +0000

    updated product page and layout

--- pages/Commerce/cart.vue
+++ pages/Commerce/cart.vue
@@ -1,14 +1,163 @@
 <template>
-  <NuxtLayout
-    :back-href="paths.category"
-    :back-label-desktop="$t('backToShopping')"
-    :back-label-mobile="$t('back')"
-    :heading="$t('myCart')"
-  >
-    <CartPageContent />
-  </NuxtLayout>
+  <div class="cartPage">
+    <form action="">
+      <v-row>
+        <v-col cols="8">
+          <v-row>
+            <v-col cols="12">
+              <v-card min-width="500px" style="padding-left: 15px; padding-bottom: 15px;">
+                <v-toolbar color="transparent" density="compact" title="Items (2 Items)">
+                  <v-btn prepend-icon="fas fa-trash">Remove All</v-btn>
+                </v-toolbar>
+                <v-table fixed-header>
+                  <thead>
+                    <tr>
+                      <th class="text-left">
+                        Image
+                      </th>
+                      <th class="text-left">
+                        Product Name
+                      </th>
+                      <th class="text-left">
+                        Price
+                      </th>
+                      <th class="text-left">
+                        Color
+                      </th>
+                      <th class="text-left">
+                        Size
+                      </th>
+                      <th class="text-left">
+                        Quantity
+                      </th>
+                      <th class="text-left">
+                        Action
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <v-avatar image="https://cdn.vuetifyjs.com/images/john.jpg" rounded="0" size="70"></v-avatar>
+                      </td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td>
+                        <v-text-field type="number" label="Quantity" hide-details="auto">
+                        </v-text-field>
+                      </td>
+                      <td>
+                        <v-btn icon="fas fa-xmark" size="x-small"></v-btn>
+                      </td>
+                    </tr>
+                  </tbody>
+                </v-table>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field clearable label="Add Coupon"></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+              <v-card style="padding-left: 15px; padding-bottom: 15px;">
+                <v-tabs v-model="tab" bg-color="transparent">
+                  <v-tab value="one">Billing Address</v-tab>
+                  <v-tab value="two">Shipping Address</v-tab>
+                </v-tabs>
+
+                <v-card-text>
+                  <v-window v-model="tab">
+                    <v-window-item value="one">
+                      <billingaddress />
+                    </v-window-item>
+
+                    <v-window-item value="two">
+                      <shippingaddress />
+                    </v-window-item>
+                  </v-window>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+
+        <v-col cols="4">
+          <v-row>
+            <v-card min-width="500px" style="padding-left: 15px; padding-bottom: 15px;">
+              <v-col cols="12">
+                <v-toolbar color="transparent" density="compact" title="Order Summary"></v-toolbar>
+                <v-table fixed-header>
+                  <tr>
+                    <th>Subtotal</th>
+                    <td>$ </td>
+                  </tr>
+                  <v-divider></v-divider>
+                  <tr>
+                    <th>Discount</th>
+                    <td>- ( $ )</td>
+                  </tr>
+                  <v-divider></v-divider>
+                  <tr>
+                    <th>Shipping</th>
+                    <td></td>
+                  </tr>
+                  <v-divider></v-divider>
+                  <tr>
+                    <th>
+                      <h5 style="color: orange;">Total Price</h5>
+                    </th>
+                    <td>$ </td>
+                  </tr>
+                </v-table>
+              </v-col>
+
+              <v-col cols="12">
+                <v-toolbar color="transparent" density="compact" title="Checkout"></v-toolbar>
+
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field label="Cardholder Name"></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field type="number" label="Card Number"></v-text-field>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-text-field type="date" label="Expiration Date"></v-text-field>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-text-field type="number" label="CVC"></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-btn width="100%" color="orange">Pay Now</v-btn>
+                  </v-col>
+                </v-row>
+
+              </v-col>
+            </v-card>
+          </v-row>
+        </v-col>
+      </v-row>
+    </form>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script>
+  import billingaddress from '../../../components/Pages/commerce/billingAddress.vue'
+  import shippingaddress from '../../../components/Pages/commerce/shippingAddress.vue'
 
+  export default {
+    components: {
+      billingaddress,
+      shippingaddress
+    }
+  }
+</script>
+
+<script setup>
+  useHead({
+    title: 'Shopping Cart',
+  })
 </script>
--- pages/Product/[slug].vue
+++ pages/Product/[slug].vue
@@ -1,11 +1,87 @@
 <template>
-  <div>
+  <div class="contentPage">
     <v-row>
       <v-col cols="12">
-        <v-row>
-          <v-col cols="6"><productPageGallery /></v-col>
-          <v-col cols="6"><productDetails /></v-col>
-        </v-row>
+        <section data-bs-version="5.1" class="pricing1 cid-twaUcnOkam" id="pricing1-4h">
+          <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(255, 255, 255);"></div>
+
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-md-6">
+                <v-carousel>
+                  <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
+
+                  <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
+
+                  <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
+                </v-carousel>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="right">
+                  <div class="card">
+                    <p class="mbr-fonts-style display-4">
+                      By: <a href="`/account/user/`">Seller</a></p>
+                  </div>
+                  <p class="name mbr-fonts-style display-2"><strong>Diamond Ring</strong></p>
+
+                  <div class="price-line">
+                    <p class="desc2 mbr-fonts-style display-5"><s>$145.00</s></p>
+                    <p class="plus1 mbr-fonts-style display-5">
+                      <strong>&nbsp;</strong>$135.00</p>
+                  </div>
+                  <p class="mbr-text mbr-fonts-style display-4">Dicta sunt explicabo. Nemo enim
+                    ipsam voluptatem
+                    voluptas sit odit aut fugit, sed quia consequuntur. Lorem ipsum nonum eirmod dolor.
+                    <br>
+                    <br>Aquia sit amet, elitr, sed diam nonum eirmod tempor invidunt labore et dolore magna
+                    aliquyam.erat, sed diam voluptua. At vero accusam et justo duo dolores et ea rebum. Stet clitain
+                    vidunt ut labore eirmod tempor invidunt magna aliquyam. Stet clitain vidunt ut labore.</p>
+
+                  <v-card variant="text" class="productVCard">
+                    <v-card-actions>
+                      <v-list>
+                        <v-list-item>
+                          <p class="desc mbr-fonts-style display-7"><strong>Size:</strong>&nbsp;<v-btn
+                              variant="outlined">S</v-btn>
+                          </p>
+                        </v-list-item>
+
+                        <v-list-item>
+                          <p class="desc mbr-fonts-style display-7"><strong>Color:</strong>&nbsp;<v-btn variant="text">
+                              <v-avatar color="surface-variant"></v-avatar>
+                            </v-btn>
+                          </p>
+                        </v-list-item>
+                      </v-list>
+                    </v-card-actions>
+                  </v-card>
+
+                  <div class="mbr-section-btn"><a class="btn btn-secondary display-7" href=""><span
+                        class="mobi-mbri mobi-mbri-plus mbr-iconfont mbr-iconfont-btn"></span>Add to List</a> <a
+                      class="btn btn-secondary display-7" href=""><span
+                        class="mobi-mbri mobi-mbri-shopping-cart mbr-iconfont mbr-iconfont-btn"></span>Add to Basket</a>
+                    <a class="btn btn-secondary display-7" href="">Buy Now</a></div>
+                  <div class="price-line1">
+                    <p class="desc mbr-fonts-style display-7"><strong>
+                        Category:</strong></p>
+                    <p class="plus mbr-fonts-style display-4">
+                      &nbsp;Rings</p>
+                  </div>
+                  <div class="price-line1">
+                    <p class="desc mbr-fonts-style display-7"><strong>
+                        Tags:</strong>&nbsp;</p>
+                    <p class="plus mbr-fonts-style display-4">ring</p>
+                  </div>
+                  <div class="price-line1">
+                    <p class="desc mbr-fonts-style display-7"><strong>
+                        Product ID:</strong></p>
+                    <p class="plus mbr-fonts-style display-4">2234</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </v-col>
 
       <v-col cols="12">
@@ -48,13 +124,13 @@
 </template>
 
 <script>
-import productPageGallery from '../../components/Pages/commerce/product/productPageGallery.vue'
-import productDetails from '../../components/Pages/commerce/product/productDetails.vue'
-import productInfo from '../../components/Pages/commerce/product/productInfo.vue'
-import comments from '../../components/user/comments.vue'
-import productSpecs from '../../components/Pages/commerce/product/productSpecs.vue'
-import productFaqs from '../../components/Pages/commerce/product/productFaqs.vue'
-import productCompare from '../../components/Pages/commerce/product/productCompare.vue'
+  import productPageGallery from '../../components/Pages/commerce/product/productPageGallery.vue'
+  import productDetails from '../../components/Pages/commerce/product/productDetails.vue'
+  import productInfo from '../../components/Pages/commerce/product/productInfo.vue'
+  import comments from '../../components/user/comments.vue'
+  import productSpecs from '../../components/Pages/commerce/product/productSpecs.vue'
+  import productFaqs from '../../components/Pages/commerce/product/productFaqs.vue'
+  import productCompare from '../../components/Pages/commerce/product/productCompare.vue'
 
   export default {
     components: {
@@ -72,8 +148,7 @@ import productCompare from '../../components/Pages/commerce/product/productCompa
 </script>
 
 <script setup>
-
-definePageMeta({
+  definePageMeta({
     layout: 'nolive',
   });
 
