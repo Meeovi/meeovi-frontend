584261455fa67d98ff651c120cacc2ff0e4981ab
Sebastian hilton <bastianh86@outlook.com>
2024-11-06 00:28:40 +0000

    removed prismic cms and its folders

--- server/api/commerce/catalog/products/recentlyviewed/recentlyviewed.js
+++ server/api/commerce/catalog/products/recentlyviewed/recentlyviewed.js
@@ -1,27 +1,47 @@
-import { eventHandler } from 'h3'
-import { PrismaClient } from '@prisma/client'
+import {
+  eventHandler
+} from 'h3'
+import {
+  PrismaClient
+} from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 export default eventHandler(async (event) => {
   if (event.method === 'GET') {
     try {
-      const bestsellers = await prisma.mgtn_report_viewed_product_aggregated_daily.findMany({
-        orderBy: [
-          { views_num: 'desc' },
-          { rating_pos: 'desc' }
+      const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000)
+
+      const recentlyViewed = await prisma.mgtn_report_viewed_product_aggregated_daily.findMany({
+        where: {
+          period: {
+            gte: oneDayAgo
+          }
+        },
+        orderBy: [{
+            views_num: 'desc'
+          },
+          {
+            rating_pos: 'desc'
+          }
         ],
         take: 10
       })
 
-      return bestsellers
+      return recentlyViewed
     } catch (error) {
-      console.error('Error fetching bestsellers:', error)
-      throw createError({ statusCode: 500, statusMessage: 'Error fetching bestsellers data' })
+      console.error('Error fetching recently viewed:', error)
+      throw createError({
+        statusCode: 500,
+        statusMessage: 'Error fetching recently viewed data'
+      })
     } finally {
       await prisma.$disconnect()
     }
   } else {
-    throw createError({ statusCode: 405, statusMessage: `Method ${event.method} Not Allowed` })
+    throw createError({
+      statusCode: 405,
+      statusMessage: `Method ${event.method} Not Allowed`
+    })
   }
 })
